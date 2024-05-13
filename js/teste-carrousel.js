661cb6a7721c3d97c41e5340c2787cc2ce0f6874
Paiaro <gabrielpaiarop@gmail.com>
2024-05-13 18:52:26 -0300

    carousel

--- js/teste-carrousel.js
+++ js/teste-carrousel.js
@@ -1,21 +1,7 @@
-let slideIndex = 0;
-showSlides(slideIndex);
 
-function moveSlide(n) {
-    showSlides(slideIndex += n);
-}
+const myCarouselElement = document.querySelector('#myCarousel')
 
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("carousel-images")[0].getElementsByTagName("img");
-    if (n >= slides.length) {
-        slideIndex = 0;
-    }
-    if (n < 0) {
-        slideIndex = slides.length - 1;
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex].style.display = "block";
-}
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false
+})
